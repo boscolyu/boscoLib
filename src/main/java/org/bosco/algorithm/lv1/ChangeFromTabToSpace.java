17a3f9cd6a08ad19526ebe906acbb5766691e614
heetae.lyu <heetae.lyu@nhnent.com>
2015-12-13 01:56:40 +0900

    make env for http://codingdojang.com/scode/405

---
+++ src/main/java/org/bosco/algorithm/lv1/ChangeFromTabToSpace.java
@@ -0,0 +1,8 @@
+package org.bosco.algorithm.lv1;
+
+public class ChangeFromTabToSpace {
+
+	public String changeFromTabToSpace(String source) {
+		return source.replaceAll("\t", "    ");
+	}
+}
---
+++ src/test/java/org/bosco/algorithm/lv1/ChangeFromTabToSpaceTest.java
@@ -0,0 +1,19 @@
+package org.bosco.algorithm.lv1;
+
+import org.junit.Test;
+
+public class ChangeFromTabToSpaceTest {
+
+	
+	@Test
+	public void testChangeFromTabToSpace() {
+		
+		String source = "";
+		ChangeFromTabToSpace changer = new ChangeFromTabToSpace() ;
+		String output = changer.changeFromTabToSpace(source);
+		
+		
+		
+		
+	}
+}
