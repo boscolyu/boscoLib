68eb79700f5f478a6881898991ef2b33112c99ed
heetae.lyu <heetae.lyu@nhnent.com>
2015-12-18 23:59:39 +0900

    [boscolyu/boscoLib/#3]  update the sort test code

---
+++ src/main/java/org/bosco/algorithm/lv1/Sort.java
@@ -0,0 +1,34 @@
+package org.bosco.algorithm.lv1;
+
+public class Sort {
+
+	public int[] insertsortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+
+	public int[] selectionsortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+
+	public int[] bubblesortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+
+	public int[] mergesortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+
+	public int[] heapsortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+
+	public int[] quicksortAscending(int[] notSortArray) {
+		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		return sortAscendingArray;
+	}
+}
---
+++ src/test/java/org/bosco/algorithm/lv1/SortTest.java
@@ -0,0 +1,74 @@
+package org.bosco.algorithm.lv1;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import junit.framework.Assert;
+
+public class SortTest {
+
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Test
+	public void testInsertSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.insertsortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+
+	@Test
+	public void testSelectionSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.selectionsortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+	
+	
+	@Test
+	public void testBubbleSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.bubblesortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+	
+	
+	@Test
+	public void testMergeSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.mergesortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+	
+	@Test
+	public void testHeapSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.heapsortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+	
+	@Test
+	public void testQuickSort() {
+		int[] notSortArray = { 5, 100, 23, 2123, 3, 234, 34, 234, 654, 3423, 342};
+		Sort sort = new Sort();
+		int[] sortArray = sort.quicksortAscending(notSortArray);
+		Assert.assertTrue(isSorted(sortArray));
+	}
+	
+	
+	private boolean isSorted(int[] sortArray) {
+		int prevValue = 0; 
+		for (int value : sortArray) {
+			if (prevValue > value) {
+				return false;
+			}
+			prevValue = value;
+		}
+		return true;
+	}
+}
