9de9b9a831c862a6dc3fdb976562253d51b5c403
heetae.lyu <heetae.lyu@nhnent.com>
2016-05-20 22:23:55 +0900

    implement the insertion sort

--- src/main/java/org/bosco/algorithm/lv1/Sort.java
+++ src/main/java/org/bosco/algorithm/lv1/Sort.java
@@ -3,8 +3,19 @@ package org.bosco.algorithm.lv1;
 public class Sort {
 
 	public int[] insertsortAscending(int[] notSortArray) {
-		int[] sortAscendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		return sortAscendingArray;
+		int[] sourceArray = notSortArray.clone();
+		for (int i = 1; i < sourceArray.length; i++) {
+			int temp = sourceArray[i];
+			for (int j = 0; j < i; j++) {
+				if (temp < sourceArray[j]) {
+					int temp2 = sourceArray[j];
+					sourceArray[j] = temp;
+					temp = temp2;
+				}
+			}
+			sourceArray[i] = temp;
+		}
+		return sourceArray;
 	}
 
 	public int[] selectionsortAscending(int[] notSortArray) {
